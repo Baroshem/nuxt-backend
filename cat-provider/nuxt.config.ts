7bddf241fd91eafe31b0300df8df78e52e3efb84
Baroshem <jakub.andrzejewski.dev@gmail.com>
2023-10-17 09:43:15 +0200

    feat: nuxt-security

--- cat-provider/nuxt.config.ts
+++ cat-provider/nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase', 'nuxt-security'],
 
-  routeRules: {
-    '/**' : {
-      cors: false
+  security: {
+    corsHandler: {
+        origin: '*'
     }
-  }
+  },
 })
